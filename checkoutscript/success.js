1038981ec0d976463918094120decaadd01b6cfc
Geetu Rana <geeturana07@gmail.com>
2022-05-08 15:52:53 +0530

    2 more pages added

---
+++ checkoutscript/success.js
@@ -0,0 +1,10 @@
+import { footer } from "../components/footer.js";
+document.getElementById("footer").innerHTML = footer();
+
+let orderid = document.getElementById("orderNo");
+orderid.innerText ="PO"+(Math.random()*100000000000000).toFixed(0);
+
+
+document.getElementById("home").addEventListener("click",(()=>{
+    window.location.href ="index.html";
+}))
